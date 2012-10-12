3b71c7a775f2b77445aeb7bcf1eee8f2cc90fecd
pichenettes <ol.gillet@gmail.com>
2012-10-12 17:31:49 +0200

    stmlib update

--- blink/blink.cc
+++ blink/blink.cc
@@ -27,14 +27,14 @@
 // Default interrupt handlers.
 extern "C" {
 
-void HardFault_Handler(void) { while (1); }
-void MemManage_Handler(void) { while (1); }
-void BusFault_Handler(void) { while (1); }
-void UsageFault_Handler(void) { while (1); }
-void NMI_Handler(void) { }
-void SVC_Handler(void) { }
-void DebugMon_Handler(void) { }
-void PendSV_Handler(void) { }
+void HardFault_Handler() { while (1); }
+void MemManage_Handler() { while (1); }
+void BusFault_Handler() { while (1); }
+void UsageFault_Handler() { while (1); }
+void NMI_Handler() { }
+void SVC_Handler() { }
+void DebugMon_Handler() { }
+void PendSV_Handler() { }
 
 }
 
