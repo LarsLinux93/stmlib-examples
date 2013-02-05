a626d243af3f171064004d128eb56c82a3be7dd8
pichenettes <ol.gillet@gmail.com>
2013-02-05 21:27:08 +0100

    F4 example (not tested)

---
+++ blink-f4/blink.cc
@@ -0,0 +1,76 @@
+// Copyright 2012 Olivier Gillet.
+// 
+// Author: Olivier Gillet (ol.gillet@gmail.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// See http://creativecommons.org/licenses/MIT/ for more information.
+
+#include <stm32f4xx_conf.h>
+
+// Default interrupt handlers.
+extern "C" {
+
+void HardFault_Handler() { while (1); }
+void MemManage_Handler() { while (1); }
+void BusFault_Handler() { while (1); }
+void UsageFault_Handler() { while (1); }
+void NMI_Handler() { }
+void SVC_Handler() { }
+void DebugMon_Handler() { }
+void PendSV_Handler() { }
+void SysTick_Handler() { }
+
+}
+
+/*
+extern "C" {
+
+void SysTick_Handler() {
+  static bool led_status;
+  
+  led_status = !led_status;
+  if (led_status) {
+    GPIO_SetBits(GPIOA, GPIO_Pin_0);
+  } else {
+    GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+  }
+}
+
+}
+*/
+
+void Init() {
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+      
+  GPIO_InitTypeDef gpio_init;
+  gpio_init.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+  gpio_init.GPIO_Mode = GPIO_Mode_OUT;
+  gpio_init.GPIO_OType = GPIO_OType_PP;
+  gpio_init.GPIO_Speed = GPIO_Speed_25MHz;
+  gpio_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &gpio_init);
+  GPIO_SetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2);
+  //SysTick_Config(F_CPU / 8);
+}
+
+int main(void) {
+  Init();
+  while (1) { }
+}
