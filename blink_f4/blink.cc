bed4e3da162d53f086ba6a181c1908be06d295b0
pichenettes <ol.gillet@gmail.com>
2013-02-06 11:36:24 +0100

    Blink tested on F1x and F4xx

--- blink_f4/blink.cc
+++ blink_f4/blink.cc
@@ -35,26 +35,28 @@ void NMI_Handler() { }
 void SVC_Handler() { }
 void DebugMon_Handler() { }
 void PendSV_Handler() { }
-void SysTick_Handler() { }
 
 }
 
-/*
 extern "C" {
 
 void SysTick_Handler() {
-  static bool led_status;
+  static uint8_t divider = 0;
+  static bool led_status = false;
   
-  led_status = !led_status;
-  if (led_status) {
-    GPIO_SetBits(GPIOA, GPIO_Pin_0);
-  } else {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+  if ((++divider) == 0) {
+    if (led_status) {
+      GPIO_SetBits(GPIOA, GPIO_Pin_1);
+      led_status = false;
+    } else {
+      GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+      led_status = true;
+    }
   }
 }
 
 }
-*/
+
 
 void Init() {
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
@@ -66,8 +68,8 @@ void Init() {
   gpio_init.GPIO_Speed = GPIO_Speed_25MHz;
   gpio_init.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOA, &gpio_init);
-  GPIO_SetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2);
-  //SysTick_Config(F_CPU / 8);
+  GPIO_SetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_2);
+  SysTick_Config(F_CPU / 1024);
 }
 
 int main(void) {
